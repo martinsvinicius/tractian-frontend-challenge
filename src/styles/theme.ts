2744a69f594c30725f5550028e74bf41d0318651
vinicius5g <vinicius.victor.sm@gmail.com>
2021-09-30 20:25:29 -0300

    theming

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,24 @@
 import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
+  colors: {
+    gray: {
+      50: '#f4f4f5',
+    },
+    blue: {
+      700: '#1e3a8a',
+      600: '#224db6',
+      500: '#0066ff',
+      200: '#60a5fa',
+    },
+    green: {
+      300: '#22c55e',
+    },
+    yellow: {
+      500: '#f9cd49',
+    },
+  },
+
   fonts: {
     heading: 'Roboto',
     body: 'Roboto',
