4ec43ae73baec298e317e40421472f1f8428bfda
vinicius5g <vinicius.victor.sm@gmail.com>
2021-10-03 12:09:27 -0300

    hook to get assets

---
+++ src/services/hooks/assets/useAssets.ts
@@ -0,0 +1,15 @@
+import { useQuery } from 'react-query';
+import { Asset } from '../../../models/Asset';
+import { api } from '../../api';
+
+export async function getAssets(): Promise<Asset[]> {
+  const { data } = await api.get<Asset[]>('/assets');
+
+  return data;
+}
+
+export function useAssets() {
+  return useQuery('assets', getAssets, {
+    staleTime: 1000 * 60 * 5, //5 minutes
+  });
+}
