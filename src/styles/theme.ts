72c21578b88f89507ee0ec7efd1f112c4557c0d3
vinicius5g <vinicius.victor.sm@gmail.com>
2021-10-03 12:09:09 -0300

    adding theme breakpoints

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,13 @@
 import { extendTheme } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
+
+const breakpoints = createBreakpoints({
+  sm: '30em',
+  md: '48em',
+  lg: '62em',
+  xl: '80em',
+  '2xl': '96em',
+});
 
 export const theme = extendTheme({
   colors: {
@@ -23,4 +32,6 @@ export const theme = extendTheme({
     heading: 'Roboto',
     body: 'Roboto',
   },
+
+  breakpoints
 });
